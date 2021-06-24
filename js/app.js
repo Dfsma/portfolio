65d7f8066c725d2f81e24fcc8e7c01c89f3006a6
Diego <diegof.df58@gmail.com>
2021-06-23 21:11:09 -0400

    add dark styles

---
+++ js/app.js
@@ -0,0 +1,20 @@
+/*const btn = document.querySelector(".btn-toggle");
+const siteTitle = document.getElementById("site-title");
+const navTitles = document.getElementById("nav-style");
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+  document.body.classList.add("dark-theme");
+  siteTitle.style["color"] = "white";
+  navTitles.style["color"] = "white";
+}
+
+btn.addEventListener("click", function () {
+  document.body.classList.toggle("dark-theme");
+
+  let theme = "light";
+  if (document.body.classList.contains("dark-theme")) {
+    theme = "dark";
+  }
+  localStorage.setItem("theme", theme);
+});*/
